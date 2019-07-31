27cba25ce84ebfefb5d391e9e96abea0116eb644
snyder <snyderjosephd@yahoo.com>
2019-07-31 09:20:36 -0500

    Added button to start new game

--- src/main.js
+++ src/main.js
@@ -1,28 +1,38 @@
-generateBoard(squares);
 
-const board = document.querySelector('.board');
+const newGameButton = document.querySelector('.newGame');
 
-board.addEventListener('click', (event) => {
-    const clickedItemClass = event.target.classList[0];
-    const clickedItem = event.target;
-    const squareNumberString = event.target.classList[1].split('__')[1];
-    const squareNumber = parseInt(squareNumberString, 10);
-    
-    if (clickedItemClass === 'square') {
-        const squareElement = clickedItem.childNodes[1];
-    
-        // Iterate over squares array and update the value of the appropriate square
-        squares.forEach(square => {
-            if (square.id === squareNumber && square.value === '') {
-                let nextSymbol = nextMark();
-                squareElement.innerHTML = nextSymbol;
-                square["value"] = nextSymbol;
-            }
-        });
+newGameButton.addEventListener('click', (event) => {
+    const board = document.querySelector('.board');
 
-        checkGameOver();
-        changePlayer();
+    players[0].turn = true;
+    players[1].turn = false;
 
-    }
+    squares.forEach(square => {
+        square.value = '';
+    });
 
+    board.innerHTML = '';
+    generateBoard(squares);
+
+    board.addEventListener('click', (event) => {
+        const clickedItemClass = event.target.classList[0];
+        const clickedItem = event.target;
+        const squareNumberString = event.target.classList[1].split('__')[1];
+        const squareNumber = parseInt(squareNumberString, 10);
+
+        if (clickedItemClass === 'square') {
+            const squareElement = clickedItem.childNodes[1];
+
+            // Iterate over squares array and update the value of the appropriate square
+            squares.forEach(square => {
+                if (square.id === squareNumber && square.value === '') {
+                    let nextSymbol = nextMark();
+                    squareElement.innerHTML = nextSymbol;
+                    square["value"] = nextSymbol;
+                    checkGameOver();
+                    changePlayer();
+                }
+            });
+        }
+    });
 });
